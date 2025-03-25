44fcb580ffc4f2850e2ecb3b987f9b627c82cfde
roggoz <reaganroggoz@gmail.com>
2025-03-26 00:47:53 +0300

    third day

---
+++ frontend/src/components/Common/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t py-12">
+      <div className="container mx-auto grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0">
+        <div>
+          <h3 className="text-lg">Newsletter</h3>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
