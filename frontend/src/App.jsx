1f1d8b27682316734af1c977775035a06bf26ed0
roggoz <reaganroggoz@gmail.com>
2025-03-31 23:55:10 +0300

    Seventh day updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<UserLayout />}>
-          {/* user Layout */}
+          <Route index element={<Home />} />
         </Route>
-        <Route>{/* user Layout */}</Route>
+        <Route>{/* admin Layout */}</Route>
       </Routes>
     </BrowserRouter>
   );
