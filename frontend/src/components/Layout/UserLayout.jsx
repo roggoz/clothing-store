3d2f99479f9536f2d6a6a448919426902b8ef9f3
roggoz <reaganroggoz@gmail.com>
2025-04-01 14:11:02 +0300

    layout updates

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Common/Header";
 import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayout = () => {
   return (
@@ -9,7 +10,9 @@ const UserLayout = () => {
       <Header />
 
       {/* Main content */}
-
+      <main>
+        <Outlet />
+      </main>
       {/* footer */}
       <Footer />
     </>
