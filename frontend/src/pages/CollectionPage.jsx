baa99e357588aadb5c5e9a79ae1dfec00c67f74a
roggoz <reaganroggoz@gmail.com>
2025-04-28 11:51:45 +0300

    updates

--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -1,9 +1,118 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { FaFilter } from "react-icons/fa";
+import FilterSidebar from "../components/Products/FilterSidebar";
+import SortOptions from "../components/Products/SortOptions";
+import ProductGrid from "../components/Products/ProductGrid";
+import ProductDetails from "../components/Products/ProductDetails";
 
 const CollectionPage = () => {
-  return (
-    <div>CollectionPage</div>
-  )
-}
+  const [products, setProducts] = useState([]);
+  const SidebarRef = useRef(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-export default CollectionPage
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClickOutside = (e) => {
+    //Close sidebar if clicked outside
+    if (SidebarRef.current && !SidebarRef.current.contains(e.target)) {
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Add Event listner for clicks
+    document.addEventListener("mousedown", handleClickOutside);
+    // clean event listener
+    document.removeEventListener("mousedown", handleClickOutside);
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      const fetchedProducts = [
+        {
+          _id: 1,
+          name: "Product 1",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=3" }],
+        },
+        {
+          _id: 2,
+          name: "Product 2",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=4" }],
+        },
+        {
+          _id: 3,
+          name: "Product 3",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=5" }],
+        },
+        {
+          _id: 4,
+          name: "Product 4",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=6" }],
+        },
+        {
+          _id: 5,
+          name: "Product 5",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=7" }],
+        },
+        {
+          _id: 6,
+          name: "Product 6",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=8" }],
+        },
+        {
+          _id: 7,
+          name: "Product 7",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=9" }],
+        },
+        {
+          _id: 8,
+          name: "Product 8",
+          price: 100,
+          images: [{ url: "https://picsum.photos/500/500?random=10" }],
+        },
+      ];
+      setProducts(fetchedProducts);
+    }, 1000);
+  }, []);
+  return (
+    <div className="flex flex-col lg:flex-row">
+      {/* Mobile Filter button */}
+      <button
+        onClick={toggleSidebar}
+        className="lg:hidden border p-2 flex justify-center items-center"
+      >
+        <FaFilter className="mr-2 " />
+      </button>
+
+      {/* Filter Sidebar */}
+      <div
+        ref={SidebarRef}
+        className={`${
+          isSidebarOpen ? "translate-0" : "-translate-x-full"
+        } fixed inset-y-0 z-50 left-0 w-64 bg-white overflow-y-auto transition-transform duration-300 lg:static lg:translate-x-0`}
+      >
+        <FilterSidebar />
+      </div>
+      <div className="flex-grow p-4">
+        <h2 className="text-2xl uppercase mb-4">All Collection</h2>
+
+        {/* Sort option */}
+        <SortOptions />
+
+        {/* Product Grid */}
+        <ProductGrid products={products} />
+      </div>
+    </div>
+  );
+};
+
+export default CollectionPage;
