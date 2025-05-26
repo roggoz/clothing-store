eb289c7fb98a2912aff29fc59313d334a5fdafc4
roggoz <reaganroggoz@gmail.com>
2025-05-26 14:33:58 +0300

    Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,9 @@ import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
 import Checkout from "./components/Cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
 
 export const App = () => {
   return (
@@ -23,7 +26,14 @@ export const App = () => {
           <Route path="collections/:collection" element={<CollectionPage />} />
           <Route path="products/:id" element={<ProductDetails />} />
           <Route path="checkout" element={<Checkout />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="my-orders" element={<MyOrdersPage />} />
         </Route>
+
         <Route>{/* admin Layout */}</Route>
       </Routes>
     </BrowserRouter>
