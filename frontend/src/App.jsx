a10179e197f26b72db8c37b459315a47785a68f6
roggoz <reaganroggoz@gmail.com>
2025-06-15 15:20:28 +0300

    App jsx file at the end of frontend developement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,9 @@ import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
+import EditProductPage from "./components/Admin/EditProductPage";
+import OrderManagement from "./components/Admin/OrderManagement";
 
 export const App = () => {
   return (
@@ -41,6 +44,9 @@ export const App = () => {
           {/* admin Layout */}
           <Route index element={<AdminHomePage />} />
           <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+          <Route path="products/:id/edit" element={<EditProductPage />} />
+          <Route path="orders" element={<OrderManagement />} />
         </Route>
       </Routes>
     </BrowserRouter>
